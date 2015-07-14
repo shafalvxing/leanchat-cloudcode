4e539053b50581f72fb008f3edb824496dfb68f0
xiecuifeng <542172663>
2015-07-14 18:25:49 +0800

    提交声音文件名称
    提交声音文件名称

--- cloud/mchat.js
+++ cloud/mchat.js
@@ -16,7 +16,7 @@ function getPushMessage(params) {
   var contentStr = params.content;
   var json = {
     badge: "Increment",
-    sound: "default"
+    sound: "cheering.caf"
 //    ,"_profile": "dev"      //设置证书，开发时用 dev，生产环境不设置
   };
   var msg = JSON.parse(contentStr);
