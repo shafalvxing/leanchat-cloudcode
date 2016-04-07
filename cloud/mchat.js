e0793d97403b1314def9d015c483f57ee94c9b61
luoxiaorong <414143303@qq.com>
2016-04-07 22:36:38 +0800

    短信触发回退

--- cloud/mchat.js
+++ cloud/mchat.js
@@ -65,7 +65,7 @@ function receiversOffline(req, res) {
       var pushMessage = getPushMessage(req.params);
       console.log('pushMessage :' + pushMessage + ' offlinePeers:' + req.params.offlinePeers);
       //对方离线发送短信
-      sofaSendMsg(req.params.fromPeer,req.params.offlinePeers[0],req.params.content,req.params.convId);
+      // sofaSendMsg(req.params.fromPeer,req.params.offlinePeers[0],req.params.content,req.params.convId);
       res.success({pushMessage: pushMessage});
     } catch(err) {
       // json parse error
@@ -92,65 +92,65 @@ function conversationAdd(req,res){
   console.log('conversationAdd');
   res.success();
 }
-    function sofaSendMsg(fromPeer,offlinePeer,content,convId){
-        var dataStr = "{" + '\"fromPeer\":' + '\"' + fromPeer + "\","
-            + '\"offlinePeer\":' + '\"' + offlinePeer + "\","
-            + '\"content\":' + '\"' + content + "\","
-            + '\"convId\":' + '\"' + convId + "\"}";
-        var data = {};
-        data["bizParams"] = dataStr;
-        sofaJsonp({
-            url: "http://www.shafalvxing.com/jsonp/sendInTimeMsgTip.do",              //请求地址
-            data: data,        //请求参数
-            callback:"callback",
-            time:1000
-        });
-    }
+    // function sofaSendMsg(fromPeer,offlinePeer,content,convId){
+    //     var dataStr = "{" + '\"fromPeer\":' + '\"' + fromPeer + "\","
+    //         + '\"offlinePeer\":' + '\"' + offlinePeer + "\","
+    //         + '\"content\":' + '\"' + content + "\","
+    //         + '\"convId\":' + '\"' + convId + "\"}";
+    //     var data = {};
+    //     data["bizParams"] = dataStr;
+    //     sofaJsonp({
+    //         url: "http://www.shafalvxing.com/jsonp/sendInTimeMsgTip.do",              //请求地址
+    //         data: data,        //请求参数
+    //         callback:"callback",
+    //         time:1000
+    //     });
+    // }
 
-     function sofaJsonp(options) {
-        options = options || {};
-        if (!options.url || !options.callback) {
-            throw new Error("参数不合法");
-        }
-        //创建 script 标签并加入到页面中
-        var callbackName = ('jsonp_' + Math.random()).replace(".", "");
-        var oHead = window.document.getElementsByTagName('head')[0];
-        options.data[options.callback] = callbackName;
-        var params = sofaFormatParams(options.data);
-        var oS = window.document.createElement('script');
-        oHead.appendChild(oS);
+    //  function sofaJsonp(options) {
+    //     options = options || {};
+    //     if (!options.url || !options.callback) {
+    //         throw new Error("参数不合法");
+    //     }
+    //     //创建 script 标签并加入到页面中
+    //     var callbackName = ('jsonp_' + Math.random()).replace(".", "");
+    //     var oHead = window.document.getElementsByTagName('head')[0];
+    //     options.data[options.callback] = callbackName;
+    //     var params = sofaFormatParams(options.data);
+    //     var oS = window.document.createElement('script');
+    //     oHead.appendChild(oS);
 
-        //创建jsonp回调函数
-        window[callbackName] = function (json) {
-            oHead.removeChild(oS);
-            clearTimeout(oS.timer);
-            window[callbackName] = null;
-            options.success && options.success(json);
-        };
+    //     //创建jsonp回调函数
+    //     window[callbackName] = function (json) {
+    //         oHead.removeChild(oS);
+    //         clearTimeout(oS.timer);
+    //         window[callbackName] = null;
+    //         options.success && options.success(json);
+    //     };
 
-        //发送请求
-        oS.src = options.url + '?' + params;
+    //     //发送请求
+    //     oS.src = options.url + '?' + params;
 
-        //超时处理
-        if (options.time) {
-            oS.timer = setTimeout(function () {
-                window[callbackName] = null;
-                oHead.removeChild(oS);
-                options.fail && options.fail({ message: "超时" });
-            }, options.time);
-        }
-    };
+    //     //超时处理
+    //     if (options.time) {
+    //         oS.timer = setTimeout(function () {
+    //             window[callbackName] = null;
+    //             oHead.removeChild(oS);
+    //             options.fail && options.fail({ message: "超时" });
+    //         }, options.time);
+    //     }
+    // };
 
-    //格式化参数
-    function sofaFormatParams(data) {
-        var arr = [];
-        for (var name in data) {
-            // arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
-            arr.push(name + "=" + data[name]);
-        }
-        arr.push(("v=" + Math.random()).replace(".",""));
-        return arr.join("&");
-    }
+    // //格式化参数
+    // function sofaFormatParams(data) {
+    //     var arr = [];
+    //     for (var name in data) {
+    //         // arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
+    //         arr.push(name + "=" + data[name]);
+    //     }
+    //     arr.push(("v=" + Math.random()).replace(".",""));
+    //     return arr.join("&");
+    // }
 
 exports.messageReceived = messageReceived;
 exports.receiversOffline = receiversOffline; // used by main.js
